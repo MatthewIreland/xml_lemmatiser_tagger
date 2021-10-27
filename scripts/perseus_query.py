f9b75217e39e8875a58675a4c07552d763c30ed1
Matthew Ireland <matthew@ireland.cx>
2021-10-27 22:36:45 +0100

    Python: improve error handling/logging, and retry the Perseus request once if it initially fails

--- scripts/perseus_query.py
+++ scripts/perseus_query.py
@@ -1,8 +1,8 @@
 import betacode.conv
-import urllib
+import urllib.request
 import re
 import sys
-import xmltodict
+import time
 
 
 class PerseusAnalysis:
@@ -13,22 +13,47 @@ class PerseusAnalysis:
     def __get_perseus_analysis(self, greek_word):
         greek_word_encoded = urllib.parse.quote_plus(greek_word)
 
-        file = urllib.request.urlopen(f"https://www.perseus.tufts.edu/hopper/morph?l={greek_word_encoded}&la=greek")
-        data = file.read().decode("utf=8")
-        file.close()
+        try:
+            file = urllib.request.urlopen(f"https://www.perseus.tufts.edu/hopper/morph?l={greek_word_encoded}&la=greek")
+            data = file.read().decode("utf=8")
+            file.close()
+        except Exception:
+            try:
+                sys.stderr.write(f"Error looking up {greek_word_encoded}. Retrying...\n")
+                time.sleep(60)
+                file = urllib.request.urlopen(f"https://www.perseus.tufts.edu/hopper/morph?l={greek_word_encoded}&la=greek")
+                data = file.read().decode("utf=8")
+                file.close()
+            except Exception as e:
+                sys.stderr.write(f"Retry failed.\n")
+                raise e
+
 
         matches = re.findall('<td class="greek">(.*)</td>.*\n.*<td>(.*)</td>', data)
 
         lemmata = []
         pos_tags = []
 
+        num_pos_tags = 0
         for match in matches:
             pos_tags.append(match[1].split(" ")[0])
+            num_pos_tags += 1
+
+        if num_pos_tags == 0:
+            with open('errors.txt', 'a') as f:
+                f.write(f"{word}    {unicode_greek_word}    can't find pos tags\n")
+
 
         matches = re.findall('<h4 class="greek">(.*)</h4>', data)
 
+        num_lemmata = 0
         for match in matches:
             lemmata.append(match)
+            num_lemmata += 1
+
+        if num_lemmata == 0:
+            with open('errors.txt', 'a') as f:
+                f.write(f"{word}    {unicode_greek_word}    can't find lemma\n")
 
         self.__lemmata = list(set(lemmata))
         self.__pos_tags = list(set(pos_tags))
@@ -60,6 +85,6 @@ if __name__ == "__main__":
     except Exception:
         unicode_greek_word = betacode.conv.beta_to_uni(word)
         print(unicode_greek_word)
-        with open('errors.txt', 'w') as f:
-            f.write(f"{word}    {unicode_greek_word}\n")
+        with open('errors.txt', 'a') as f:
+            f.write(f"{word}    {unicode_greek_word}    unknown error\n")
         sys.stderr.write(f"Cannot find lemma for word {word}\n")
