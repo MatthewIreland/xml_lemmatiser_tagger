90923c1b60ffe7ba5b8eaeb906793921b0ac26d1
Matthew Ireland <matthew@ireland.cx>
2022-02-08 15:50:05 +0000

    Script to prepare and do pre-processing on corpus

---
+++ prepare_corpus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf run/in
+cp -r data/whole_corpus run/
+mv run/whole_corpus run/in
+
+
+find ./run/in -type f -name "*.xml" -print0 | xargs -0 dos2unix
+
+find ./run/in -name '*.xml' -exec sed -i "s/<\/p>//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/<p>//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/<\/sp>//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/<sp>//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/&lt;//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/&gt;//g" {} +
+find ./run/in -name '*.xml' -exec sed -i "s/—//g" {} +
+
